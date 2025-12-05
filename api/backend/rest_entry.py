c37497aa8968a00fc9e35fccb1e0316a4e018af1
micahcheng <micahcheng333@gmail.com>
2025-12-04 20:20:22 -0500

    stuff

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -8,6 +8,10 @@ from backend.db_connection import db
 from backend.simple.simple_routes import simple_routes
 from backend.ngos.ngo_routes import ngos
 
+from backend.admin.admin_routes import admin_bp
+
+
+
 def create_app():
     app = Flask(__name__)
 
@@ -50,6 +54,7 @@ def create_app():
     app.logger.info("create_app(): registering blueprints with Flask app object.")
     app.register_blueprint(simple_routes)
     app.register_blueprint(ngos, url_prefix="/ngo")
+    app.register_blueprint(admin_bp, url_prefix="/admin")
 
     # Don't forget to return the app object
     return app
