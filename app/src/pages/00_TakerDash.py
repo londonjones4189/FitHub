65abc73da57ca6d8cae9451b34666c1e6a85b603
William Cramer <cramer.w@northeastern.edu>
2025-12-07 03:47:58 -0500

    Fixed prefix

--- app/src/pages/00_TakerDash.py
+++ app/src/pages/00_TakerDash.py
@@ -11,7 +11,7 @@ st.set_page_config(layout="wide")
 
 SideBarLinks()
 
-API_BASE = "http://api:4000/d"
+API_BASE = "http://api:4000/t"
 
 st.title(f"Welcome Taker, {st.session_state['first_name']}!")
 st.write('')
